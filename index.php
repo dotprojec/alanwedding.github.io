7f6daf6ee9e24cb37a2a0f382ca6417703fcf7c2
dotprojec <90308216+dotprojec@users.noreply.github.com>
2021-09-10 18:03:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,72 @@
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <title>Send an Email</title>
 </head>
 <body>
-<br/><br/><br/>
-<div class="row">
-<div class="col-md-4"></div>
-<div class="col-md-4">
-<form action='kirim.php' method='post'>
-<div class="form-group">
-<label >Nama</label>
-<input type="text" class="form-control" name="nama">
-</div>
-<div class="form-group">
-<label >Nomer HP</label>
-<input type="text" class="form-control" name="hp">
-</div>
-<div class="form-group">
-<label >Alamat</label>
-<input type="text" class="form-control" name="alamat">
-</div>
-<div class="form-group">
-<label >Email</label>
-<input type="text" class="form-control" name="usermail">
-</div>
-<button type='Submit'>Submit</button>
-</form>
-</div>
-</div>
+
+	<center>
+		<h4 class="sent-notification"></h4>
+
+		<form id="myForm">
+			<h2>Send an Email</h2>
+
+			<label>Name</label>
+			<input id="name" type="text" placeholder="Enter Name">
+			<br><br>
+
+			<label>Email</label>
+			<input id="email" type="text" placeholder="Enter Email">
+			<br><br>
+
+			<label>Subject</label>
+			<input id="subject" type="text" placeholder=" Enter Subject"> 
+			<br><br>
+
+			<p>Message</p>
+			<textarea id="body" rows="5" placeholder="Type Message"></textarea>
+			<br><br>
+
+			<button type="button" onclick="sendEmail()" value="Send An Email">Submit</button> 
+		</form>
+	</center>
+
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript">
+        function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var subject = $("#subject");
+            var body = $("#body");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       subject: subject.val(),
+                       body: body.val()
+                   }, success: function (response) {
+                        $('#myForm')[0].reset();
+                        $('.sent-notification').text("Message Sent Successfully.");
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+    </script>
+
 </body>
 </html>
